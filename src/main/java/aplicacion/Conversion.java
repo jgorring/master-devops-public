345208c8de6976f03e250ea478003a452b79a3ed
operador <operador@192.168.1.48>
2019-07-23 10:36:35 +0200

    tests

---
+++ src/main/java/aplicacion/Conversion.java
@@ -0,0 +1,12 @@
+package aplicacion;
+
+public class Conversion {
+
+	public double tempConversion (double temperature, String unit) {
+		if (unit.equals("F")) {
+			return (temperature - 32) * (5.0/9.0);
+		} else {
+			return (temperature * (5.0/9.0)) + 32;
+		}
+	}
+}
---
+++ src/test/java/aplicacion/ConversionTest.java
@@ -0,0 +1,20 @@
+package aplicacion;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ConversionTest {
+
+	@Test
+	public void testTempConversion() throws Throwable {
+		
+		Conversion underTest = new Conversion();
+		
+		double temperature = 80.0d;
+		String unit = "";
+		double result = underTest.tempConversion(temperature, unit);
+		
+		assertEquals(176.0d, result, 0.0);
+	}
+}
